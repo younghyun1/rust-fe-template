e6598d9e9a60111e76aa479dc0ae879c53db253c
지영현/전임 <younghyun.chi@genesisnest.com>
2025-02-18 17:02:15 +0900

    leptos init

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,38 @@ struct AppState {
 
 #[component]
 fn AppShell() -> impl IntoView {
-    view! { <div style="background:black; color:white;">example text</div> }
+    let (response, set_response) = create_signal(String::new());
+    let on_click = move |_| {
+        spawn_local(async move {
+            let client = reqwest::Client::new();
+            match client.get("http://localhost:3000").send().await {
+                Ok(resp) => match resp.text().await {
+                    Ok(text) => set_response(text),
+                    Err(err) => set_response(format!("Error reading response: {}", err)),
+                },
+                Err(err) => set_response(format!("Request failed: {}", err)),
+            }
+        });
+    };
+
+    view! {
+        <div style="background:black; color:white;">
+            <TitleBar/>
+            <div>
+                <button on:click=on_click>"Fetch Data"</button>
+            </div>
+            <div>
+                {move || response.get()}
+            </div>
+        </div>
+    }
 }
 
 #[component]
 fn TitleBar() -> impl IntoView {
-    let (state, set_state) = signal(AppState { logged_in: false });
-
-    view! {}
+    view! {
+        <div style="font-size: 24px; font-weight: bold;">
+            "Modern Leptos Page"
+        </div>
+    }
 }
