d22734ab0fbcd4dac5f19328e9ab9d03f9048ad2
지영현/전임 <younghyun.chi@genesisnest.com>
2025-02-18 16:57:55 +0900

    leptos init

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use leptos::prelude::*;
+
+fn main() {
+    leptos::mount::mount_to_body(AppShell)
+}
+
+#[derive(Clone)]
+struct AppState {
+    logged_in: bool,
+}
+
+#[component]
+fn AppShell() -> impl IntoView {
+    view! { <div style="background:black; color:white;">example text</div> }
+}
+
+#[component]
+fn TitleBar() -> impl IntoView {
+    let (state, set_state) = signal(AppState { logged_in: false });
+
+    view! {}
+}
